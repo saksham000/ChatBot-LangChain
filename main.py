ff0ffe9a62d7dcc2a30d91d38fb4b3a99bdd10ae
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-12-06 21:44:56 +0530

    removed openai key

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-from langchain.llms import OpenAI
-import os 
-os.environ["OPENAI_API_KEY"]="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"
-llm=OpenAI()
-print(llm("Why earth is not flat ?"))
-print(os.environ['OPENAI_API_KEY'])
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt Template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Plase response to user queries"),
+        ("user", "Question:{question}")
+    ]
+)
+
+## streamlit framework
+
+st.title('Langchain Demo with OPENAI API')
+input_text=st.text_input("Search the topic u want")
+
+# OpenAI LLM
+
